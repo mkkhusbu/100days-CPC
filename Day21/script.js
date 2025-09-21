521753da4213a8f1fbcf1129ff2739ca52535851
Arif Ajmal Khusbu <arifajmalkhusbu@gmail.com>
2025-09-21 23:23:49 +0600

    Add files via upload

---
+++ Day21/script.js
@@ -0,0 +1,32 @@
+console.log(typeof null);
+
+console.log(null === undefined)
+
+// const numbers = [10, 5,20, 8, 1, 7]
+// function myFun (arr){
+//     let len = arr.length;
+//     let min = Infinity;
+//     while(len--){
+//         if(arr[len] < min){
+//           min = arr[len];
+//         }
+
+//     }
+//     return min;
+// }
+// console.log(myFun(numbers));
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango","Kiwi"];
+// console.log(fruits);
+
+//  fruits.copyWithin(2,0,3);
+// console.log(fruits);
+
+// let letters = ["A","B","C","D","E"];
+// let test= '';
+
+// // for(let x of letters.entries()){
+// //     test+= x + " ";
+// // }
+// //console.log(test);
+//
