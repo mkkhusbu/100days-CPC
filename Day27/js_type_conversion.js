299969e9815c3e005ca0da6c7b7a1451f6cb3d56
Arif Ajmal Khusbu <arifajmalkhusbu@gmail.com>
2025-09-27 21:16:43 +0600

    Add files via upload

---
+++ Day27/js_type_conversion.js
@@ -0,0 +1,23 @@
+// !=============String To number=========
+let s = "121";
+console.log(s)//number
+console.log(typeof s)//string
+let n = Number(s);
+console.log(n)//121
+console.log(typeof n);//number
+
+
+
+// !=============>>Number To String<<<=========
+var num = 123;
+var chng = String(num);
+console.log(typeof chng)
+
+// !=============> Boolean To Number <=========
+var bool = true;
+console.log(typeof bool)//boolean
+var N = Number(bool)
+console.log(N)//1
+console.log("MK")
+
+
