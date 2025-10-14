854fa1779e14b5a963ce18a1949f238dc9b82d83
Arif Ajmal Khusbu <arifajmalkhusbu@gmail.com>
2025-10-14 23:33:25 +0600

    Add files via upload

---
+++ Day34/practiceQuestion.js
@@ -0,0 +1,54 @@
+// 1st practice
+let carName="Volvo";
+
+
+// 2nd practice
+let firstName = "jhon",
+    lastName = "Doe",
+    age = 35;
+
+// 3rd practice
+let x = 10,y = 5;
+x*=y;
+
+// 4th practice
+let length = 16; // Number 
+var lastName = "Johnson"; // String
+
+var x = {
+  firstName: "John",  
+  lastName: "Doe"
+};  
+ 
+// 5th practice
+function myFunction() {
+    alert("Hello World!");
+  }
+  myFunction();
+
+// 6th practice
+var person ={
+    name ="Jhon",
+    age =50
+};
+alert(person.name +" is " + person.age);
+
+// 7th practice
+<button onclick="myFunction()">Click me.</button> 
+
+// 8th practice
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert(cars.length);
+
+const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+Brand.unshift("Ford");
+
+// 9th practice
+Math.random();
+Math.max(10,20);
+Math.sqrt(9);
+// 10th practice
+var x = 10,y =5;
+alert(x > y);
+
+(age < 18) ? "Too young" : "Old enough";
